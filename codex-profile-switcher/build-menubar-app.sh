27f4d85c079befeba1c5d1bf5e73fe3999781c14
hd2yao <1054290355@qq.com>
2026-07-09 12:45:15 +0800

    chore: bump menu bar app version

--- codex-profile-switcher/build-menubar-app.sh
+++ codex-profile-switcher/build-menubar-app.sh
@@ -36,9 +36,9 @@ cat > "$CONTENTS_DIR/Info.plist" <<'PLIST'
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>CFBundleShortVersionString</key>
-  <string>0.7.1</string>
+  <string>0.8.0</string>
   <key>CFBundleVersion</key>
-  <string>12</string>
+  <string>13</string>
   <key>LSMinimumSystemVersion</key>
   <string>13.0</string>
   <key>LSUIElement</key>
